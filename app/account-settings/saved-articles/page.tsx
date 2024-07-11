708034b26d0fb450b2eb1280f7f7a988346e83cf
HumptyDumpy01 <tuznikolas@gmail.com>
2024-07-11 13:39:05 +0300

    Separated sidebar links onto 1 versatile component.

---
+++ app/account-settings/saved-articles/page.tsx
@@ -0,0 +1,12 @@
+// 'use client';
+
+/*interface SavedArticlesInterface {
+  // children: ReactNode;
+}*/
+export default function SavedArticles(/*{  }: SavedArticlesInterface*/) {
+  return (
+    <>
+      <h1>Saved Articles</h1>
+    </>
+  );
+}
