048b6387aa740e7158e8562fa04fab52d7a00550
HumptyDumpy01 <tuznikolas@gmail.com>
2024-07-15 16:26:01 +0300

    Added watermark dummy image template for tours gallery if the actual amount of images less than 4. The UI would still look constent.

--- components/UI/Gallery/Gallery.tsx
+++ components/UI/Gallery/Gallery.tsx
@@ -4,6 +4,7 @@ import './Gallery.scss';
 import { useCartDispatch } from '@/store/hooks';
 import { tourSliceActions } from '@/store/tourSlice';
 import { StaticImageData } from 'next/image';
+import watermarkImage from '@/assets/images/viatours-watermark-logo.svg';
 
 export type GalleryType = {
   info: {
@@ -16,45 +17,39 @@ export type GalleryType = {
 export default function Gallery({ info }: GalleryType) {
   const dispatch = useCartDispatch();
 
-  if (!info.images) {
-    throw new Error(`Invalid gallery images: ${info.images}`);
+  if (!info.images || info.images.length === 0) {
+    throw new Error(`The images array should contain at least 1 image!`);
   }
 
-  const mainImage = info.images[0];
-  const restOfImages = info.images.slice(1, 4);
-  /*
-    // when clicking on Esc, close the gallery slider
-    window.addEventListener(`keydown`, (e) => {
-      if (e.key === `Escape`) {
-        dispatch(tourSliceActions.setGallerySliderVisibility(false));
-      }
-    });*/
+  // Create a new array from the images prop to avoid direct mutation
+  const imagesToShow = Array.isArray(info.images) ? [...info.images] : [info.images];
+  while (imagesToShow.length < 4) {
+    imagesToShow.push(watermarkImage); // Fill with watermarkImage if less than 5 images
+  }
+
+  // Use the first image as the main image and the rest as secondary images
+  const mainImage = imagesToShow[0];
+  const restOfImages = imagesToShow.slice(1, 4);
 
   return (
     <>
       <div className="description__gallery">
         <div className="description__gallery-images grid">
           <div className="description__gallery-img-1">
-            {/*// @ts-ignore*/}
-            <img onClick={() => dispatch(tourSliceActions.setGallerySliderVisibility(true))} src={mainImage.src}
-                 alt={info.title} />
+            <img onClick={() => dispatch(tourSliceActions.setGallerySliderVisibility(true))}
+              // @ts-ignore
+                 src={mainImage.src} alt={info.title} />
           </div>
-
           <div className="description__gallery-images-container-1 grid grid-two-cols">
-            {/*// @ts-ignore*/}
-            {restOfImages.map((image, index) => {
-              return (
-                <div key={index} className={`description__gallery-img-${index + 2}`}>
-                  {/*// @ts-ignore*/}
-                  <img onClick={() => dispatch(tourSliceActions.setGallerySliderVisibility(true))} src={image.src}
-                       alt={info.title}
-                       className="description__gallery-img-2" />
-                </div>
-              );
-            })}
+            {restOfImages.map((image, index) => (
+              <div key={index} className={`description__gallery-img-${index + 2}`}>
+                <img onClick={() => dispatch(tourSliceActions.setGallerySliderVisibility(true))}
+                  // @ts-ignore
+                     src={image.src || image} alt={info.title} className="description__gallery-img-2" />
+              </div>
+            ))}
           </div>
         </div>
-        {/*<!--    <span class="description__gallery&#45;&#45;see-all">See all photos</span>-->*/}
         <span onClick={() => dispatch(tourSliceActions.setGallerySliderVisibility(true))}
               className="description__gallery--see-all">See all photos</span>
       </div>
