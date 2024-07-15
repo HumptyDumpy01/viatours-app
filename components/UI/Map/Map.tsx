5ee336247323000f03b11ec70cbc36ece21e0806
HumptyDumpy01 <tuznikolas@gmail.com>
2024-07-15 11:39:11 +0300

    Partially fixed the issues with the public API exposed. My APIs are not visible in a code but they are in http requests..

--- components/UI/Map/Map.tsx
+++ components/UI/Map/Map.tsx
@@ -3,16 +3,11 @@ import './Map.scss';
 import MapComponent from '@/components/UI/Map/MapComponent';
 
 // Example static latitude and longitude for demonstration
-const exampleLat = 48.8584; // Eiffel Tower latitude
-const exampleLng = 2.2945; // Eiffel Tower longitude
-
 export default function GoogleMap() {
   return (
     <MapComponent
       apiKey={String(process.env.GOOGLE_MAP_API_KEY)}
       mapId={String(process.env.GOOGLE_MAP_ID)}
-      lat={exampleLat}
-      lng={exampleLng}
     />
   );
 }
--- components/UI/Map/MapComponent.tsx
+++ components/UI/Map/MapComponent.tsx
@@ -1,49 +1,48 @@
 // MapComponent.tsx
 'use client';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './Map.scss';
 import { AdvancedMarker, APIProvider, Map, Pin } from '@vis.gl/react-google-maps';
 
 type MapComponentType = {
   mapId: string;
   apiKey: string;
-  lat: number; // New prop for latitude
-  lng: number; // New prop for longitude
 }
 
 type Poi = { key: string, location: google.maps.LatLngLiteral }
+const locations: Poi[] = [
+  { key: 'operaHouse', location: { lat: -33.8567844, lng: 151.213108 } },
+  { key: 'tarongaZoo', location: { lat: -33.8472767, lng: 151.2188164 } },
+  { key: 'manlyBeach', location: { lat: -33.8209738, lng: 151.2563253 } },
+  { key: 'hyderPark', location: { lat: -33.8690081, lng: 151.2052393 } },
+  { key: 'theRocks', location: { lat: -33.8587568, lng: 151.2058246 } },
+  { key: 'circularQuay', location: { lat: -33.858761, lng: 151.2055688 } },
+  { key: 'harbourBridge', location: { lat: -33.852228, lng: 151.2038374 } },
+  { key: 'kingsCross', location: { lat: -33.8737375, lng: 151.222569 } },
+  { key: 'botanicGardens', location: { lat: -33.864167, lng: 151.216387 } },
+  { key: 'museumOfSydney', location: { lat: -33.8636005, lng: 151.2092542 } },
+  { key: 'maritimeMuseum', location: { lat: -33.869395, lng: 151.198648 } },
+  { key: 'kingStreetWharf', location: { lat: -33.8665445, lng: 151.1989808 } },
+  { key: 'aquarium', location: { lat: -33.869627, lng: 151.202146 } },
+  { key: 'darlingHarbour', location: { lat: -33.87488, lng: 151.1987113 } },
+  { key: 'barangaroo', location: { lat: -33.8605523, lng: 151.1972205 } }
+];
+const PoiMarkers = (props: { pois: Poi[] }) => {
+  return (
+    <>
+      {props.pois.map((poi: Poi) => (
+        <AdvancedMarker
+          key={poi.key}
+          position={poi.location}>
+          {/*<Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />*/}
+          <Pin background={'#ef5b1b'} glyphColor={'#fff'} borderColor={'#fff'} />
+        </AdvancedMarker>
+      ))}
+    </>
+  );
+};
 
-export default function MapComponent({ mapId, apiKey, lat, lng }: MapComponentType) {
-  const [locations, setLocations] = useState<Poi[]>([]);
-
-  useEffect(() => {
-    const fetchLocations = async () => {
-      const response = await fetch(`/api/maps?lat=${lat}&lng=${lng}`);
-      const data = await response.json();
-      // Assuming the API returns an array of locations with { key, lat, lng }
-      const pois = data.map((item, index) => ({
-        key: `poi-${index}`,
-        location: { lat: item.lat, lng: item.lng }
-      }));
-      setLocations(pois);
-    };
-
-    fetchLocations();
-  }, [lat, lng]);
-
-  const PoiMarkers = (props: { pois: Poi[] }) => {
-    return (
-      <>
-        {props.pois.map((poi: Poi) => (
-          <AdvancedMarker
-            key={poi.key}
-            position={poi.location}>
-            <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
-          </AdvancedMarker>
-        ))}
-      </>
-    );
-  };
+export default function MapComponent({ mapId, apiKey }: MapComponentType) {
 
   return (
     <APIProvider apiKey={apiKey}>
@@ -53,7 +52,7 @@ export default function MapComponent({ mapId, apiKey, lat, lng }: MapComponentTy
           <div id="map">
             <Map
               defaultZoom={13}
-              defaultCenter={{ lat, lng }}
+              defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
               mapId={mapId}>
               <PoiMarkers pois={locations} />
             </Map>
