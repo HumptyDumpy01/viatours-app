cdb5202165ba662d4aed37784b7427b0a47a574e
HumptyDumpy01 <tuznikolas@gmail.com>
2024-07-17 21:06:53 +0300

    Added a good meta to tour description page.

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,6 +1,8 @@
 // 'use client';
 
 import TourDescriptionSection from '@/components/tourDescription/TourDescription';
+import { DUMMY_TOURS } from '@/data/DUMMY_TOURS';
+import { notFound } from 'next/navigation';
 
 interface TourDescriptionInterface {
   params: {
@@ -8,6 +10,23 @@ interface TourDescriptionInterface {
   };
   // children: ReactNode;
 }
+
+export async function generateMetadata({ params }: TourDescriptionInterface) {
+  // const meal = FUNC_FETCHING_ITEM(params.YOUR_DYNAMIC_ID);
+  const currTour = DUMMY_TOURS.find((item) => item.id === params.id);
+
+  if (!currTour) {
+    notFound();
+  }
+  return {
+    title: `${currTour.title}`,
+    description: `${currTour.overview}. The Tour to ${currTour.city} is ${currTour.duration} long and costs ${currTour.price.adult} per person,
+    ${currTour.price.youth} per youth, and ${currTour.price.children} per children. 
+    This particular tour is rated ${currTour.rating.overall} stars by our customers, and also includes: ${currTour.what_included.green.join(`, `)}. We did not 
+    for forget about the ${currTour.what_included.orange.join(`, `)} and more! Book now!`
+  };
+}
+
 
 export default function TourDescription({ params }: TourDescriptionInterface) {
   return (
