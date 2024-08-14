25cb1ab0985e693816ed259453b5079917c12b5f
HumptyDumpy01 <tuznikolas@gmail.com>
2024-08-14 18:29:41 +0300

    Removed unnecessary imperative code for checking if viewport < 570px and instead applied simple media query.(wishlist page, skeleton UI)

--- components/account-settings/skeletons/UserWishlistSkeletons.tsx
+++ components/account-settings/skeletons/UserWishlistSkeletons.tsx
@@ -5,30 +5,10 @@
 }*/
 
 import UserWishlistSkeleton from '@/components/account-settings/skeletons/UserWishlistSkeleton';
-import { useEffect, useState } from 'react';
 
 export default function UserWishlistItemsSkeletons(/*{  }: UserWishlistItemsType*/) {
-
-  const [applyJustifyCenter, setApplyJustifyCenter] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (window.innerWidth < 570) {
-      setApplyJustifyCenter(true);
-    } else {
-      setApplyJustifyCenter(false);
-    }
-  }, []);
-
-  window.addEventListener('resize', () => {
-    if (window.innerWidth < 570) {
-      setApplyJustifyCenter(true);
-    } else {
-      setApplyJustifyCenter(false);
-    }
-  });
-
   return (
-    <div className={`wishlist-items grid ${applyJustifyCenter ? `justify-items-center` : ``}`}>
+    <div className={`wishlist-items-skeleton grid`}>
       <UserWishlistSkeleton />
       <UserWishlistSkeleton />
       <UserWishlistSkeleton />
