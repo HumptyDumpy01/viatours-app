4fb92329fe5126eba1e9769156e43e018ce962dc
HumptyDumpy01 <tuznikolas@gmail.com>
2024-08-15 21:09:52 +0300

    Paginated user orders correctly.

--- components/account-settings/contents/user-tour-purchases/UserTourPurchases.tsx
+++ components/account-settings/contents/user-tour-purchases/UserTourPurchases.tsx
@@ -1,10 +1,11 @@
 // 'use client';
 import './UserTourPurchases.scss';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SortBy from '@/components/UI/SortBy/SortBy';
 import { UserOrdersType } from '@/components/account-settings/AccountSettingsContainer';
 import UserOrder from '@/components/account-settings/contents/user-tour-purchases/UserOrder';
 import { motion } from 'framer-motion';
+import Pagination from '@/components/UI/Pagnation/Pagination';
 
 type UserTourPurchasesType = {
   userOrders: UserOrdersType[] | [];
@@ -23,6 +24,20 @@ const container = {
 };
 
 export default function UserTourPurchases({ userOrders }: UserTourPurchasesType) {
+
+  const [filteredOrderItems, setFilteredOrderItems] = useState<UserOrdersType[]>(userOrders);
+  const [originalOrderItems, setOriginalOrderItems] = useState<UserOrdersType[]>([...userOrders]);
+  const [userOrderItems, setUserOrderItems] = useState<UserOrdersType[]>(userOrders);
+  const orderItemsPerPage = 4;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const indexOfLastWishlistItem = currentPage * orderItemsPerPage;
+  const indexOfFirstNotification = indexOfLastWishlistItem - orderItemsPerPage;
+
+
+  useEffect(() => {
+    setUserOrderItems(filteredOrderItems.slice(indexOfFirstNotification, indexOfLastWishlistItem));
+  }, [currentPage, filteredOrderItems]);
 
   function handleSortUserOrders() {
   }
@@ -54,13 +69,20 @@ export default function UserTourPurchases({ userOrders }: UserTourPurchasesType)
         initial="hidden"
         animate="show"
         className="tour-purchases__cards-wrapper">
-        {userOrders.map(function(order) {
+        {userOrderItems.length > 0 && userOrderItems.map(function(order) {
           return (
-            <UserOrder counter={userOrders.length} key={order._id} order={order} />
+            <UserOrder counter={userOrderItems.length} key={order._id} order={order} />
           );
         })}
+        {userOrderItems.length === 0 && (
+          <div className="tour-purchases__no-orders">
+            <p>No tour purchases yet! Feel free to explore our tours and book your next adventure.</p>
+          </div>
+        )}
 
       </motion.div>
+      <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} totalItems={filteredOrderItems.length}
+                  itemsPerPage={orderItemsPerPage} />
     </motion.div>
   );
 }
