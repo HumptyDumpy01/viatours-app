e161be2a02c5cec2c79d43b542fc8d28d705092b
HumptyDumpy01 <tuznikolas@gmail.com>
2024-08-17 10:41:58 +0300

    Saving changes before trying to solve scrolling to the top is not happening on loading screen issue.

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -3,7 +3,6 @@
 /* IMPORTANT: DO NOT MODIFY */
 
 // IMPORTANT: FOR PRODUCTION
-/*
 import TourDescriptionSection from '@/components/tourDescription/TourDescription';
 import { notFound } from 'next/navigation';
 import { TourInterface } from '@/data/DUMMY_TOURS';
@@ -88,14 +87,13 @@ export default async function TourDescription({ params }: TourDescriptionInterfa
 
   return (
     <>
-      {/!*@ts-ignore*!/}
+      {/*@ts-ignore*/}
       <TourDescriptionSection userName={sessionVar.user.name} userEmail={sessionVar.user.email} session={sessionVar}
                               similarTours={similarTours}
                               tour={currTour} params={params} />
     </>
   );
 }
-*/
 
 /* INFO: the reason why I do have two versions is that in build bundle I change the way how
 *   I parse the data by using separate fetch apis. It is not possible to do so without env variable
@@ -103,6 +101,7 @@ export default async function TourDescription({ params }: TourDescriptionInterfa
 * */
 
 /* IMPORTANT: FOR DEVELOPMENT */
+/*
 
 import TourDescriptionSection from '@/components/tourDescription/TourDescription';
 import { notFound } from 'next/navigation';
@@ -179,7 +178,7 @@ export default async function TourDescription({ params }: TourDescriptionInterfa
   }
   return (
     <>
-      {/*@ts-ignore*/}
+      {/!*@ts-ignore*!/}
       <TourDescriptionSection session={sessionVar} userEmail={sessionVar.user.email} userName={sessionVar.user.name}
                               similarTours={similarTours}
                               tour={currTour}
@@ -187,5 +186,6 @@ export default async function TourDescription({ params }: TourDescriptionInterfa
     </>
   );
 }
+*/
 
 
