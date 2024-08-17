2bb761fb2ae964bc20765f6b1f0daea4a18e10ad
HumptyDumpy01 <tuznikolas@gmail.com>
2024-08-17 08:00:47 +0300

    Modified tour-description page.tsx, so now I do need to switch between dev mode and build mode for application to work.

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,3 +1,9 @@
+/* IMPORTANT: DO NOT MODIFY */
+/* IMPORTANT: DO NOT MODIFY */
+/* IMPORTANT: DO NOT MODIFY */
+
+// IMPORTANT: FOR PRODUCTION
+/*
 import TourDescriptionSection from '@/components/tourDescription/TourDescription';
 import { notFound } from 'next/navigation';
 import { TourInterface } from '@/data/DUMMY_TOURS';
@@ -82,10 +88,100 @@ export default async function TourDescription({ params }: TourDescriptionInterfa
 
   return (
     <>
-      {/*@ts-ignore*/}
+      {/!*@ts-ignore*!/}
       <TourDescriptionSection userName={sessionVar.user.name} userEmail={sessionVar.user.email} session={sessionVar}
                               similarTours={similarTours}
                               tour={currTour} params={params} />
     </>
   );
 }
+*/
+
+
+/* IMPORTANT: FOR DEVELOPMENT(BUGGY, BUT IT IS NOT THAT IMPORTANT. BUILD VERSION WORKS FLAWLESSLY.) */
+
+import TourDescriptionSection from '@/components/tourDescription/TourDescription';
+import { notFound } from 'next/navigation';
+import { getTourById, getTours } from '@/lib/mongodb';
+import { TourInterface } from '@/data/DUMMY_TOURS';
+import { getServerSession } from 'next-auth';
+import { authConfig } from '@/lib/auth';
+import TourDescriptionLoadingPage from '@/app/tours/[id]/loading';
+
+
+export async function generateMetadata({ params }: TourDescriptionInterface) {
+  const fetchedTours = await getTour(params.id);
+  const { currTour } = fetchedTours;
+  // const { similarTours } = currTour;
+
+  if (!currTour) {
+    notFound();
+  }
+  return {
+    title: `${currTour.title}`,
+    description: `${currTour.overview}. The Tour to ${currTour.city} is ${currTour.duration} long and costs ${currTour.price.adult} per person,
+    ${currTour.price.youth} per youth, and ${currTour.price.children} per children. 
+    This particular tour is rated ${currTour.rating.overall} stars by our customers, and also includes: ${currTour.whatsIncluded.green.join(`, `)}. We did not 
+    for forget about the ${currTour.whatsIncluded.orange.join(`, `)} and more! Book now!`
+  };
+}
+
+interface TourDescriptionInterface {
+  params: {
+    id: string;
+  };
+  // children: ReactNode;
+}
+
+async function getTour(id: string): Promise<{ currTour: TourInterface, similarTours: TourInterface[] }> {
+  'use server';
+
+  const currTour = await getTourById(id) as TourInterface;
+  const similarTours = await getTours(22, { tags: { $in: currTour.tags } }) as TourInterface[];
+
+  return {
+    currTour,
+    similarTours
+  };
+}
+
+
+export default async function TourDescription({ params }: TourDescriptionInterface) {
+  const fetchedTours = await getTour(params.id);
+  const { similarTours, currTour } = fetchedTours;
+
+  const session = await getServerSession(authConfig);
+
+  if (session === undefined) {
+    return <TourDescriptionLoadingPage />;
+  }
+
+
+  let sessionVar;
+  if (session === null) {
+    sessionVar = {
+      user: {
+        email: '',
+        name: ''
+      }
+    };
+  } else {
+    sessionVar = {
+      user: {
+        email: session!.user!.email,
+        name: session!.user!.name
+      }
+    };
+  }
+  return (
+    <>
+      {/*@ts-ignore*/}
+      <TourDescriptionSection session={sessionVar} userEmail={sessionVar.user.email} userName={sessionVar.user.name}
+                              similarTours={similarTours}
+                              tour={currTour}
+                              params={params} />
+    </>
+  );
+}
+
+
