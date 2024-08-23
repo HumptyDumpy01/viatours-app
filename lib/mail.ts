16503b0eecf41b0501faf7e7626cbec95de74b1b
HumptyDumpy01 <tuznikolas@gmail.com>
2024-08-23 11:21:16 +0300

    Added one more sending email condition to mail.ts to send 2fa verifications.

--- lib/mail.ts
+++ lib/mail.ts
@@ -38,5 +38,21 @@ export async function sendVerificationCode(email: string, token: string, type: `
     `
     });
   }
+  if (type === `twoFactorAuth`) {
+    const response = await resend.emails.send({
+      from: 'onboarding@resend.dev',
+      to: email,
+      subject: 'Viatours Two-factor Authentication',
+      html: `
+      <div style="font-family: 'Montserrat', sans-serif;">
+      <h1>Two Factor Authentication</h1>
+      <p>Your 2FA code to sign in to Viatours is: <strong>${token}</strong></p>
+      <p>This code will expire in 10 minutes.</p>
+      <br>
+      <p>If you didn't request this code, you can safely ignore this email.</p>
+      </div>
+    `
+    });
+  }
 
 }
