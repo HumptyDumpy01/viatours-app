dfa903a388082fee0652db393777339b31ea085d
HumptyDumpy01 <tuznikolas@gmail.com>
2024-09-13 12:50:58 +0300

    Saving changes before implementing AI Agent.

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -7,6 +7,7 @@ import { getServerSession } from 'next-auth';
 import { authConfig } from '@/lib/auth';
 import TourDescriptionLoadingPage from '@/app/tours/[id]/loading-page';
 import { notFound } from 'next/navigation';
+import { ArticleType } from '@/components/articles/search-article/SearchArticleContainer';
 
 interface ArticleDescriptionInterface {
   params: {
@@ -72,67 +73,24 @@ export default async function ArticleDescription({ params }: ArticleDescriptionI
   /* IMPORTANT: FOR PRODUCTION */
 
   // fetch the article from the server based on url id
-  /*
-   const article: {
-     error: boolean;
-     article: ArticleType[];
-
-   } = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/get-article-details`, {
-     method: `POST`,
-     headers: {
-       'Content-Type': `application/json`
-     },
-     body: JSON.stringify({ id })
-   }).then((response) => response.json()).catch((error) => {
-     console.error(`Fetch error:`, error);
-     notFound();
-   });
-
-   if (article.error) {
-     notFound();
-   }
-   // fetch the session from the server
-   const session = await getServerSession(authConfig);
-
-   if (session === undefined) {
-     return <TourDescriptionLoadingPage />;
-   }
-
-
-   let sessionVar;
-   if (session === null) {
-     sessionVar = {
-       user: {
-         email: '',
-         name: ''
-       }
-     };
-   } else {
-     sessionVar = {
-       user: {
-         email: session!.user!.email,
-         name: session!.user!.name
-       }
-     };
-   }*/
-  // console.log(`Article: `, article.article[0]);
-
-  ///////////////////////////////////////
-
-
-  /* IMPORTANT: FOR DEVELOPMENT */
-  // @ts-ignore
-  const article: {
+ /* const article: {
     error: boolean;
-    article: ArticleDescrType[];
-  } = await getArticleDetails(id);
+    article: ArticleType[];
 
-  if (!article?.article?.[0]?._id) {
+  } = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/get-article-details`, {
+    method: `POST`,
+    headers: {
+      'Content-Type': `application/json`
+    },
+    body: JSON.stringify({ id })
+  }).then((response) => response.json()).catch((error) => {
+    console.error(`Fetch error:`, error);
+    notFound();
+  });
+
+  if (article.error) {
     notFound();
   }
-
-  // console.log(`Article: `, article.article[0]);
-
   // fetch the session from the server
   const session = await getServerSession(authConfig);
 
@@ -157,6 +115,49 @@ export default async function ArticleDescription({ params }: ArticleDescriptionI
       }
     };
   }
+  */
+  // console.log(`Article: `, article.article[0]);
+
+  ///////////////////////////////////////
+
+
+  /* IMPORTANT: FOR DEVELOPMENT */
+   // @ts-ignore
+   const article: {
+     error: boolean;
+     article: ArticleDescrType[];
+   } = await getArticleDetails(id);
+
+   if (!article?.article?.[0]?._id) {
+     notFound();
+   }
+
+   // console.log(`Article: `, article.article[0]);
+
+   // fetch the session from the server
+   const session = await getServerSession(authConfig);
+
+   if (session === undefined) {
+     return <TourDescriptionLoadingPage />;
+   }
+
+
+   let sessionVar;
+   if (session === null) {
+     sessionVar = {
+       user: {
+         email: '',
+         name: ''
+       }
+     };
+   } else {
+     sessionVar = {
+       user: {
+         email: session!.user!.email,
+         name: session!.user!.name
+       }
+     };
+   }
   // console.log(`Session: `, sessionVar);
   return (
     <>
--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -87,6 +87,7 @@ export interface TourInterface {
 }
 
 // IMPORTANT: FOR PRODUCTION
+/*
 
 import TourDescriptionSection from '@/components/tourDescription/TourDescription';
 import { notFound } from 'next/navigation';
@@ -171,7 +172,7 @@ export default async function TourDescription({ params }: TourDescriptionInterfa
 
   return (
     <>
-      {/*@ts-ignore*/}
+      {/!*@ts-ignore*!/}
       <TourDescriptionSection userName={sessionVar.user.name} userEmail={sessionVar.user.email} session={sessionVar}
                               similarTours={similarTours}
                               tour={currTour} params={params} />
@@ -179,6 +180,7 @@ export default async function TourDescription({ params }: TourDescriptionInterfa
   );
 }
 
+*/
 
 /* INFO: the reason why I do have two versions is that in build bundle I change the way how
 *   I parse the data by using separate fetch apis. It is not possible to do so without env variable
@@ -186,7 +188,6 @@ export default async function TourDescription({ params }: TourDescriptionInterfa
 * */
 
 /* IMPORTANT: FOR DEVELOPMENT */
-/*
 
 import TourDescriptionSection from '@/components/tourDescription/TourDescription';
 import { notFound } from 'next/navigation';
@@ -268,7 +269,7 @@ export default async function TourDescription({ params }: TourDescriptionInterfa
 
   return (
     <>
-      {/!*@ts-ignore*!/}
+      {/*@ts-ignore*/}
       <TourDescriptionSection session={sessionVar} userEmail={sessionVar.user.email} userName={sessionVar.user.name}
                               similarTours={similarTours}
                               tour={currTour}
@@ -276,6 +277,5 @@ export default async function TourDescription({ params }: TourDescriptionInterfa
     </>
   );
 }
-*/
 
 
