8ae12dcbe47bc6f3d5a9f20f1b1922eaa8a971ad
HumptyDumpy01 <tuznikolas@gmail.com>
2024-12-07 21:44:19 +0200

    Changed domain variable back to main branch URL

--- helpers/generics.ts
+++ helpers/generics.ts
@@ -1,2 +1,2 @@
 // export const DOMAIN = `https://viatours-web-app.lcl.host:44354`;
-export const DOMAIN = `https://feat-fixing-issues--viatours-dev.netlify.app`;
+export const DOMAIN = `https://viatours-dev.netlify.app`;
