5f622d3de02e66457b24e5f0016ff68d249bb35b
HumptyDumpy01 <tuznikolas@gmail.com>
2024-12-09 13:43:55 +0200

    Set backdrop default to false

--- store/backdropSlice.ts
+++ store/backdropSlice.ts
@@ -3,7 +3,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 const backdropSlice = createSlice({
   name: `backdrop-slice`,
   initialState: {
-    backdropOpen: true
+    backdropOpen: false
   },
   // here, by using createSlice method,
   // in reducers we specify the functions that would be
